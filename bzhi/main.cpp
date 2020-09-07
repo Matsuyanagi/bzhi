c37484862d5d00155d604fdeecc8d62fec389557
Matsuyanagi <1475814+Matsuyanagi@users.noreply.github.com>
2020-09-08 01:20:51 +0900

    _bzhi_u64()

--- bzhi/main.cpp
+++ bzhi/main.cpp
@@ -1,12 +1,21 @@
-#include <iostream>
+#include <intrin.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
+
+#include <iostream>
 #include <string>
 #include <vector>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+#include "binstr.h"
 
+int main() {
+	uint64_t a = 0xFFFF'FFFF'FFFF'FFFF;
+
+	for ( size_t i = 0; i < 64; i++ ) {
+		uint64_t result = _bzhi_u64( a, i );
+
+		std::string result_str = binstr( result );
+		printf( "%2d : %016llx : %s\n", i, result, result_str.c_str() );
+	}
+}
